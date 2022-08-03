1678c8f6e2de2efceea7f300799015c6886e4bf2
Ney179 <fernando.eng.comp2019@gmail.com>
2022-08-03 14:15:34 -0300

    metodo create implementado para categoria

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -1,5 +1,8 @@
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Categoria } from './categoria.model';
@@ -12,11 +15,25 @@ export class CategoriaService {
   baseUrl: String = environment.baseUrl;
 
   constructor(
-    private http : HttpClient
+    private http : HttpClient,
+    private _snack : MatSnackBar
     ) { }
 
     findAll(): Observable<Categoria[]> {
       const url = `${this.baseUrl}/categorias`
       return this.http.get<Categoria[]>(url);
+    }
+
+    create(categoria: Categoria): Observable<Categoria>{
+      const url = `${this.baseUrl}/categorias`
+      return this.http.post<Categoria>(url, categoria);
+    }
+
+    message(str : String): void{
+      this._snack.open(`${str}`, 'OK',{
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+        duration: 3000
+      })
     }
 }
